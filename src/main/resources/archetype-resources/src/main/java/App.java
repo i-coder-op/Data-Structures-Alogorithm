33759ed2da560e0f9dbabefc7020c6b0098004ca
i-coder-op <kaleshivam019@gmail.com>
2023-01-03 18:25:28 +0530

    First Commit

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $org.java.dsa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
