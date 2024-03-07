bdb0743a2a810a235d93176b5a1f7b93724cbaa3
i-coder-op <kaleshivam019@gmail.com>
2024-03-07 10:19:03 +0530

    ATOI Function using recursion

--- src/main/java/recursion/ATOIFunctionCPlusPlus.java
+++ src/main/java/recursion/ATOIFunctionCPlusPlus.java
@@ -1,38 +1,49 @@
 package recursion;
 
+import java.util.Stack;
+
 public class ATOIFunctionCPlusPlus {
     public static void main(String[] args) {
-        String s1 = "-0abr032";
-        int number = atoi(s1);
-        System.out.println(number);
+        String s1 = "-ban23";
+        Stack<String> stack = new Stack<>();
+        Stack<String> result = new Stack<>();
+
+        s1 = s1.trim();
+
+        String str[] = s1.split("");
+        for (int i=str.length-1;i>= 0;i--)
+            stack.push(str[i]);
+
+        String sign = stack.pop();
+        if(sign.equals("-")){
+            result.push(sign);
+        }
+
+        //Hypothesis
+        result = atoi(stack, result);
+        String resultString = "";
+        for(String element : result){
+            resultString = resultString.concat(element);
+        }
+
+        if((result.size() == 1 && result.peek().equals("-")) || resultString.isBlank()){
+            System.out.println("0");
+        }else
+            System.out.println(Integer.parseInt(resultString));
     }
 
-    private static int atoi(String str) {
-        str = str.trim();
+    private static Stack<String> atoi(Stack<String> stack, Stack<String> result) {
+        //Base Condition
+        if(stack.isEmpty() || !Character.isDigit(stack.peek().charAt(0))){
+            return result;
+        }
 
-        String temp = "";
-        for (int i=0;i<str.length();i++){
-            if(i == 0 && str.charAt(i) == '-'){
-                temp = temp.concat(String.valueOf(str.charAt(i)));
-                continue;
-            }
-            if(i != 0 && str.charAt(i) == '-'){
-                return 0;
-            }
-            if(Character.isDigit(str.charAt(i))){
-                if(Integer.parseInt(String.valueOf(str.charAt(i))) > 0 && Integer.parseInt(String.valueOf(str.charAt(i))) < 10){
-                    temp = temp.concat(String.valueOf(str.charAt(i)));
-                }
-            }else{
-                break;
-            }
-        }
-        if(temp.length() == 1 && temp.contains("-")){
-            return 0;
-        }
-        if(!temp.isBlank()){
-          return Integer.parseInt(temp);
-        }
-        return 0;
+        //Induction Step
+        String tempElement = stack.pop();
+        result.push(tempElement);
+        atoi(stack, result);
+        return result;
     }
+
+
 }
